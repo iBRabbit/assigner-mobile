e077e800dff8dda6080cd6078754fc261a7ebdc4
Felix Prima <felix.prima@binus.ac.id>
2023-04-19 22:12:55 +0700

    Login & RegisterActivity : Added Basic Validation (no DB)

--- app/src/main/java/com/google/assigner_mobile/LoginActivity.java
+++ app/src/main/java/com/google/assigner_mobile/LoginActivity.java
@@ -2,13 +2,74 @@ package com.google.assigner_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText    loginUsernameEditText,
+                loginPasswordEditText;
+
+    Button      loginSubmitButton;
+    TextView    loginDirectToRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginUsernameEditText = findViewById(R.id.loginUsernameEditText);
+        loginPasswordEditText = findViewById(R.id.loginPasswordEditText);
+
+        loginSubmitButton = findViewById(R.id.loginSubmitButton);
+        loginDirectToRegisterTextView = findViewById(R.id.loginDirectToRegisterTextView);
+
+        loginSubmitButton.setOnClickListener(this);
+        loginDirectToRegisterTextView.setOnClickListener(this);
+
+        String  username = loginUsernameEditText.getText().toString(),
+                password = loginPasswordEditText.getText().toString();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+
+        if(view == loginDirectToRegisterTextView) {
+            intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        if(view == loginSubmitButton) {
+            // Open Database
+
+            if(!validateFormInput())
+                return;
+
+            // Insert Data to Database
+
+            // Close Database
+        }
+    }
+
+    boolean validateFormInput() {
+
+        if(loginUsernameEditText.getText().toString().isEmpty()) {
+            loginUsernameEditText.setError("Username is required");
+            return false;
+        }
+
+        if(loginPasswordEditText.getText().toString().isEmpty()) {
+            loginPasswordEditText.setError("Password is required");
+            return false;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/google/assigner_mobile/RegisterActivity.java
+++ app/src/main/java/com/google/assigner_mobile/RegisterActivity.java
@@ -2,13 +2,94 @@ package com.google.assigner_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText    registerUsernameEditText,
+                registerPasswordEditText,
+                registerEmailEditText,
+                registerPhoneNumberEditText;
+
+    Button      registerSubmitButton;
+    TextView    registerDirectToLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerUsernameEditText = findViewById(R.id.registerUsernameEditText);
+        registerPasswordEditText = findViewById(R.id.registerPasswordEditText);
+        registerEmailEditText = findViewById(R.id.registerEmailEditText);
+        registerPhoneNumberEditText = findViewById(R.id.registerPhoneNumberEditText);
+
+        registerSubmitButton = findViewById(R.id.registerSubmitButton);
+        registerDirectToLoginTextView = findViewById(R.id.registerDirectToLoginTextView);
+
+        registerSubmitButton.setOnClickListener(this);
+        registerDirectToLoginTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+        if(view == registerDirectToLoginTextView) {
+            intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+        if(view == registerSubmitButton) {
+            // Open Database
+
+            if(!validateFormInput())
+                return;
+
+            // Insert Data to Database
+
+            // Close Database
+
+            finish();
+        }
+    }
+
+    boolean validateFormInput() {
+        if (registerUsernameEditText.getText().toString().isEmpty()) {
+            registerUsernameEditText.setError("Username is required");
+            registerUsernameEditText.requestFocus();
+            return false;
+        }
+
+        if (registerPasswordEditText.getText().toString().isEmpty()) {
+            registerPasswordEditText.setError("Password is required");
+            registerPasswordEditText.requestFocus();
+            return false;
+        }
+
+        if (registerEmailEditText.getText().toString().isEmpty()) {
+            registerEmailEditText.setError("Email is required");
+            registerEmailEditText.requestFocus();
+            return false;
+        }
+
+        if (registerPhoneNumberEditText.getText().toString().isEmpty()) {
+            registerPhoneNumberEditText.setError("Phone number is required");
+            registerPhoneNumberEditText.requestFocus();
+            return false;
+        }
+
+        if(registerPasswordEditText.getText().toString().length() < 6) {
+            registerPasswordEditText.setError("Password must be at least 6 characters");
+            registerPasswordEditText.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 }
