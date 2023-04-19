e1cbef33e8ae9271d956f6565ef339ad05f73673
Felix Prima <felix.prima@binus.ac.id>
2023-04-19 21:35:02 +0700

    Main Activity: Added UI and Intents.

--- app/src/main/java/com/google/assigner_mobile/MainActivity.java
+++ app/src/main/java/com/google/assigner_mobile/MainActivity.java
@@ -1,13 +1,39 @@
 package com.google.assigner_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button  mainRegisterButton,
+            mainLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainRegisterButton = findViewById(R.id.mainRegisterButton);
+        mainLoginButton = findViewById(R.id.mainLoginButton);
+        mainRegisterButton.setOnClickListener(this);
+        mainLoginButton.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        if(view == mainRegisterButton) {
+            intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        }
+
+        if(view == mainLoginButton) {
+            intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
